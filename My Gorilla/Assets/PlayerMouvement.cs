626f2f87c0a7c9240455f649abfdfb71f71575cf
MisterHF <h.florianpro@gmail.com>
2023-11-23 16:55:24 +0100

    PlayerSetup
    gravité controle jump

---
+++ My Gorilla/Assets/PlayerMouvement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class MovePlayer : MonoBehaviour
+{
+    public Rigidbody2D Player;
+
+    public float movespeed = 1f;
+    float horizontalmove;
+
+    public float jumpmove = 10f;
+
+    public bool grounded = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Player.velocity = new Vector2(horizontalmove * movespeed * Time.deltaTime, Player.velocity.y);
+
+        
+    }
+
+    public void Move (InputAction.CallbackContext context)
+    {
+        horizontalmove = context.ReadValue<Vector2>().x;
+    }
+    
+    public void Jump (InputAction.CallbackContext context) 
+    {
+        if (grounded)
+        {
+            if (context.performed)
+            {
+                //Hold down jump button = full height
+                Player.velocity = new Vector2(Player.velocity.x, jumpmove);
+
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D() 
+    {
+        grounded = true;
+    }
+    private void OnTriggerExit2D()
+    {
+        grounded = false;
+    }
+}
